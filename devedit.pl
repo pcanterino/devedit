cce599a39987c9c15d4afcb771e377ea4df31ac6
pcanterino <>
2004-08-09 13:51:28 +0000

    Dev-Editor 2.0

--- devedit.pl
+++ devedit.pl
@@ -1,7 +1,7 @@
 #!C:/Programme/Perl/bin/perl.exe -w
 
 #
-# Dev-Editor 2.0 (CVS)
+# Dev-Editor 2.0
 #
 # Dev-Editor's main program
 #
@@ -23,7 +23,7 @@ use Command;
 use Output;
 use Tool;
 
-$VERSION = '2.0 (CVS)';
+$VERSION = '2.0';
 
 # Path to configuration file
 # Change if necessary!
