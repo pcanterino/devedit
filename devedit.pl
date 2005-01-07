1ffd7f98c787b767995479114622399c6cb51801
pcanterino <>
2005-01-07 15:51:26 +0000

    Updated version number and copyright year

--- devedit.pl
+++ devedit.pl
@@ -1,7 +1,7 @@
 #!C:/Programme/Perl/bin/perl.exe -w
 
 #
-# Dev-Editor 2.2a
+# Dev-Editor 2.3
 #
 # Dev-Editor's main program
 #
@@ -23,7 +23,7 @@ use Command;
 use Output;
 use Tool;
 
-$VERSION = '2.2a';
+$VERSION = '2.3';
 
 # Path to configuration file
 # Change if necessary!
