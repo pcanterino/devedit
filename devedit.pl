f0288c6096cf43b4d5663492f7a51eedc5bf49c9
pcanterino <>
2005-04-23 07:06:52 +0000

    Updated version number to 2.3.2

--- devedit.pl
+++ devedit.pl
@@ -1,7 +1,7 @@
 #!C:/Programme/Perl/bin/perl.exe -w
 
 #
-# Dev-Editor 2.3.1
+# Dev-Editor 2.3.2
 #
 # Dev-Editor's main program
 #
@@ -23,7 +23,7 @@ use Command;
 use Output;
 use Tool;
 
-$VERSION = '2.3.1';
+$VERSION = '2.3.2';
 
 # Path to configuration file
 # Change if necessary!
